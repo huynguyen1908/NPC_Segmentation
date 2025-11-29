e3ffd88041da2687a2afb699df9a04060d71b7af
huynguyen1908 <144637476+huynguyen1908@users.noreply.github.com>
2025-11-29 10:53:09 +0700

    fix bug security

--- src/main/java/org/example/repository/ImageRepository.java
+++ src/main/java/org/example/repository/ImageRepository.java
@@ -2,10 +2,12 @@ package org.example.repository;
 
 import org.example.entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ImageRepository extends JpaRepository<Image, Integer> {
+    @Query("SELECT i.url FROM Image i WHERE i.id = :id")
     String findImageUrlById(Integer id);
 }
 
